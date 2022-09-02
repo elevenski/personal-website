9ee35f698f58706fff3796b945182a4662199c9d
Eleven <11@apptime.tech>
2022-09-02 23:45:47 +0300

    Update About.vue

--- components/About.vue
+++ components/About.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="about padding section">
 <p class="boldText"><b>Hi, I’m Eleven 👋</b><br>Self taught Full-stack web developer.</p>
-<p class="normalText opacity-60 mt-4">I’m currently working as a full-stack web developer for <a href="https://www.apptime.eu.org" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Orcsan</a>.</p>
+<p class="normalText opacity-60 mt-4">I’m currently working as a full-stack web developer for <a href="https://www.orcsan.eu.org" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Orcsan</a>.</p>
 <p class="normalText opacity-60 mt-4">I’m a 16 years old Self taught Full-stack web developer in <a href="https://www.google.com/search?q=turkey" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Turkey</a>, I usually make and share open-source projects. Also share all my open-source projects on GitHub.</p>
 <div class="aboutBtns mt-5">
 <a href="mailto:elevenvac@pm.me" class="aboutBtn rounded-full mr-2"><p class="aboutBtnText p-3 smallText opacity-80">Email</p></a>
