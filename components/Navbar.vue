21eaaa33ea11a44c1daba92cc804977e56f0e820
Eleven <11@apptime.tech>
2022-09-03 23:08:58 +0300

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,11 +3,3 @@
 <p class="navbarBtn thinText display-inline-block"><a href="/">Home</a></p>
 </div>
 </template>
-
-<script>
-  export default{
-    mounted() {
-      console.log(process.env.TOKEN)
-    }
-  }
-</script>
