3e29036b2395afc1bf5843d46397ab171b032829
Eleven <11@apptime.tech>
2023-04-29 09:05:36 +0300

    Update ImageWp.vue

--- components/ImageWp.vue
+++ components/ImageWp.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="image section">
-<img class="ImageWp" src="https://allhdwallpapers.com/wp-content/uploads/2015/06/Istanbul-4.jpg" draggable="false">
+<img class="ImageWp" src="https://cutewallpaper.org/22/istanbul-iphone-wallpapers/1876811936.jpg" draggable="false">
 <p class="smallText opacity-60 mt-4 text-center">Istanbul, Turkey</p>
 </div>
 </template>
