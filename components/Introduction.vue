8bb55be7eb1a724acf11aff39b2e426f8949f51e
Eleven <11@apptime.tech>
2023-04-29 09:04:10 +0300

    Update Introduction.vue

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="about padding section">
-<p class="boldText"><b>Hi, I’m Eleven 👋</b><br>Self taught Full-stack web developer.</p>
-<p class="normalText opacity-60 mt-4">I’m currently working as a full-stack web developer for <a href="https://www.orcsan.eu.org" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Orcsan</a>.</p>
-<p class="normalText opacity-60 mt-4">I’m a 16 years old Self taught Full-stack web developer in <a href="https://www.google.com/search?q=turkey" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Turkey</a>, I usually make and share open-source projects. Also share all my open-source projects on GitHub.</p>
+<p class="boldText"><b>Hi, I’m Eleven 👋</b><br>Self-taught web developer.</p>
+<p class="normalText opacity-60 mt-4">I’m currently working as a web developer for <a href="https://www.orcsan.eu.org" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Orcsan</a>.</p>
+<p class="normalText opacity-60 mt-4">I’m a - years old Self-taught web developer in <a href="https://www.google.com/search?q=turkey" target="_blank" class="border-gray-500 border-b-2 border-opacity-40 hover:border-opacity-70">Turkey</a>, I usually make and share open-source projects. Also share all my open-source projects on GitHub.</p>
 <a href="/about" class="generalBtn rounded-full mr-2 mt-4"><p class="generalBtnText p-3 smallText opacity-80">About</p></a>
 </div>
 </template>
